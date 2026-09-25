0dfc54645bbd0225258dcc60d69c73203a532e59
Mohammed Inham <mohmmedinham2003@gmail.com>
2026-09-25 15:01:58 +0530

    Add Node class and stack operations in DSANode

--- DSANode.java
+++ DSANode.java
@@ -1,9 +1,18 @@
 import java.*;
-class 
+class Node{
+		int data;
+		Node next;
+		Node(int data){this.data = data;}
+}
 
 class DSANode{
 	public static void main(String args[]){
-		
-		
+		Stack s1 = new Stack();
+		s1.push(100);
+		s1.push(200);
+		s1.push(300);
+		s1.push(400);
+		s1.push(500);
+		s1.printStack(); //[500, 400, 300, 200, 100]		
 	}
 }
